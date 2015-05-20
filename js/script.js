b145e19f3a02f4387e469a4bb8bb8cadcbffcca2
friskygrape <deron_li@hotmail.com>
2015-05-20 18:19:00 -0400

    Final changes before redo of website

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+
+
+// $(document).ready(function(){
+// 	console.log("can you hear me?");
+//     $(".toggle").toggle(function(){
+//         $("#hide").hide();
+//         $("#hide").show();
+//     });
+// });
+
+
